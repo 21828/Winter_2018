7169653afe6216bc6418982084899bf444dc6611
21828 <nightcondor@ukr.net>
2019-01-16 22:42:09 +0200

    Add files

--- Stepik_Functions_2/main.cpp
+++ Stepik_Functions_2/main.cpp
@@ -7,8 +7,11 @@ double length(int x1,int y1, int x2, int y2)
 }
 int main()
 {
-  int x1,x2,y1,y2;
+  int x1,x2,y1,y2, d;
+
    cin>>x1>>y1>>x2>>y2;
+
    cout<<length( x1, y1, x2, y2);
+
   return 0;
 }
