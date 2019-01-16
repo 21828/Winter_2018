b1aaab696a57a7674c0dbe41f98105a29532f4a7
21828 <nightcondor@ukr.net>
2019-01-16 21:48:19 +0200

    Added project  files

---
+++ Stepik_Functions_2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+double length(int x1,int y1, int x2, int y2)
+{
+    return sqrt(pow((x1 - x2),2) + pow((y1 - y2),2));
+}
+int main()
+{
+  int x1,x2,y1,y2;
+   cin>>x1>>y1>>x2>>y2;
+   cout<<length( x1, y1, x2, y2);
+  return 0;
+}
