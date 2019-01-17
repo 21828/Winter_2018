095911cdf26df82d2f3bd637188661db890e4738
21828 <nightcondor@ukr.net>
2019-01-17 21:48:29 +0200

    Rewrite file

--- Stepik_Funct_4/main.cpp
+++ Stepik_Funct_4/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+bool IsPointHere(double x, double y)
+{
 
+    return (sqrt(pow(x,2)+pow(y,2))<=sqrt(2) || x+y==1);
+}
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+  double x,y;
+    cin>>x>>y;
+    IsPointHere(x,y)? cout<<"YES":cout<<"NO";
+  return 0;
 }
