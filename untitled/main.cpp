ef6fa275874ac608b9020f594b13a7ca5cf8a4c9
21828 <nightcondor@ukr.net>
2019-01-17 18:52:02 +0200

    Add files

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-
 using namespace std;
+int IsPointThere(double x, double y)
+{
 
+        while (x>=-1 && x<=1 && y>=-1 && y<=1)
+        {
+            return 1;
+        }
+         return 0;
+
+
+ }
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+  double x, y;
+    cin>>x>>y;
+    switch(IsPointThere(x,y))
+    {
+    case 1:cout<<"true";
+    break;
+    case 2:cout<<"false";
+    break;
+    }
+  return 0;
 }
