82025b053ee702511b85f7ab47e8d9c0ce666a6f
21828 <nightcondor@ukr.net>
2019-01-30 22:02:08 +0200

    Add file

--- IgorD_5/main.cpp
+++ IgorD_5/main.cpp
@@ -1,23 +1,63 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
-char cnpk(char a,char b)
+ void foo(string s)
 {
-    cout<<a;
-}
+
+
+   for (int i = 0; i < 4; i++)
+   {
+        for(int j = 0; j < 4; j++)
+        {
+            for(int k = 0; k < 4; k++ )
+            {
+                for(int l = 0; l < 4; l++)
+                {
+                    cout<<s[i]<<s[j]<<s[k]<<s[l]<<endl;
+
+                }
+            }
+        }
+     }
+  }
+
 int main()
 {
+    int min;
     string s;
-    int k(0),n(0);
-    char a,b;
-    getline(cin,s);
-    a = s[k];
-    b = s[n];
-    for(int i = s.length();i!=0;i--)
-    {
+    vector <int> v(4);
 
-        cnpk(a,b);
-        k++;
+    getline(cin,s);
+
+    for(int i = 0; i < 4; i++)
+    {
+        v[i] = s[i];
     }
+
+    int n,p = 4;
+    for(int i = 0; i < 4; i++) //Проблемний цикл!!!
+    {
+        min = i;
+        for(int k = i; k < 4 ; k++)
+        {
+            if(v[k] < v[min])
+             min = k;
+        }
+
+        n = v[i];
+        v[i] = v[min];
+        v[min] = n;
+
+    }
+
+
+
+     for(int i = 0; i < 4; i++)
+    {
+        s[i] = v[i];
+    }
+     foo(s);
+
     return 0;
 }
